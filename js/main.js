fa504400b4b91d097801e88dd9f6615b7cf974c1
Boris Jurosevic <borisjurosevic001@gmail.com>
2014-05-02 16:20:47 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+// Can also be used with $(document).ready()
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
+</script>
+</head>
+
+<body >
+
+
+
+</body>
+</html>
